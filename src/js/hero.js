de9d081a115edd042ac504356d7184c01b47b098
Andrii <andreyoshlakov18051986@gmail.com>
2025-07-05 17:12:59 +0300

    change_png_on_jpg_add_hero_to_swiper

---
+++ src/js/hero.js
@@ -0,0 +1,30 @@
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+Swiper.use([Navigation]);
+
+const prevBtnHero = document.querySelector('.hero-btn-slide-prev');
+const nextBtnHero = document.querySelector('.hero-btn-slide-next');
+
+const swiperHero = new Swiper('.hero-swiper', {
+  slidesPerView: 1,
+  spaceBetween: 16,
+  loop: false,
+  navigation: {
+    nextEl: '.hero-btn-slide-next',
+    prevEl: '.hero-btn-slide-prev',
+  },
+  on: {
+    slideChange: () => {
+      const { isBeginning, isEnd } = swiperHero;
+      prevBtnHero.disabled = isBeginning;
+      nextBtnHero.disabled = isEnd;
+    },
+  },
+});
+
+// Ініціальна перевірка
+prevBtnHero.disabled = swiperHero.isBeginning;
+nextBtnHero.disabled = swiperHero.isEnd;
