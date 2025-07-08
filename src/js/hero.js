ba10f5b522d6c005a6325264151f72aeeb56fd05
Andrii <andreyoshlakov18051986@gmail.com>
2025-07-08 18:02:42 +0300

    add_buttons_on_keyboard

--- src/js/hero.js
+++ src/js/hero.js
@@ -8,15 +8,17 @@ Swiper.use([Navigation]);
 const prevBtnHero = document.querySelector('.hero-btn-slide-prev');
 const nextBtnHero = document.querySelector('.hero-btn-slide-next');
 
-
 const swiperHero = new Swiper('.hero-swiper', {
-
   slidesPerView: 1,
   spaceBetween: 16,
   loop: false,
   navigation: {
     nextEl: '.hero-btn-slide-next',
     prevEl: '.hero-btn-slide-prev',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
   },
   on: {
     slideChange: () => {
